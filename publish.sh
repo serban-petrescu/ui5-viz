a09f74a27db7a65f1319d33d5666208ea077fecc
Serban Petrescu <Serban.Petrescu@outlook.com>
2017-04-27 21:21:39 +0300

    Added bower publishing build step

---
+++ publish.sh
@@ -0,0 +1,18 @@
+if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+
+    git clone https://github.com/serban-petrescu/packaged-ui5-viz.git
+
+    cp -r dist/* packaged-ui5-viz/
+    cd packaged-ui5-viz
+
+    git remote rm origin
+    git remote add origin https://serban-petrescu:${GITHUB_TOKEN}@github.com/serban-petrescu/packaged-ui5-viz.git > /dev/null 2>&1
+
+    git add . *.*
+    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+    git tag -a "$TRAVIS_TAG" -m "$TRAVIS_TAG"
+
+    git push --follow-tags --quiet --set-upstream origin master
+fi
