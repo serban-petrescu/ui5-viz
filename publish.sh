431d775608884fdd95e1dbe7bf1396f6f9f1c410
Serban Petrescu <Serban.Petrescu@outlook.com>
2017-04-27 22:13:37 +0300

    Updated tag check to not match master

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ ! -z "$TRAVIS_TAG" ]; then
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
 
